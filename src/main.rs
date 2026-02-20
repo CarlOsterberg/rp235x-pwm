ed7953fa2e595e39606dcba499d2ab9af588ccae
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2026-02-20 22:02:11 +0100

    Create independent pwm slices on pin 6, 11, 16 and 21

--- src/main.rs
+++ src/main.rs
@@ -61,15 +61,27 @@ mod app {
     const PWM_TOP: u16 = 20000;
     const UART_READER_CAPACITY: usize = 32;
     const MSG_Q_CAPACITY: usize = 32;
-    type Pwm =
+    type PwmPin6 =
         hal::pwm::Channel<hal::pwm::Slice<hal::pwm::Pwm2, hal::pwm::FreeRunning>, hal::pwm::A>;
+
+    type PwmPin11 =
+    hal::pwm::Channel<hal::pwm::Slice<hal::pwm::Pwm4, hal::pwm::FreeRunning>, hal::pwm::A>;
+
+    type PwmPin16 =
+    hal::pwm::Channel<hal::pwm::Slice<hal::pwm::Pwm6, hal::pwm::FreeRunning>, hal::pwm::A>;
+
+    type PwmPin21 =
+    hal::pwm::Channel<hal::pwm::Slice<hal::pwm::Pwm0, hal::pwm::FreeRunning>, hal::pwm::A>;
 
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        pwm2channel_a: Pwm,
+        pwm_pin_6: PwmPin6,
+        pwm_pin_11: PwmPin11,
+        pwm_pin_16: PwmPin16,
+        pwm_pin_21: PwmPin21,
         buffer: [u8; UART_READER_CAPACITY],
         msg_q_sender: Sender<'static, u8, MSG_Q_CAPACITY>,
         msg_q_receiver: Receiver<'static, u8, MSG_Q_CAPACITY>,
@@ -100,7 +112,6 @@ mod app {
 
         let sio = Sio::new(ctx.device.SIO);
 
-        // -------------------------- UART TX(gpio0/pin1) RX(gpio1/pin2) --------------------------
         let pins = hal::gpio::Pins::new(
             ctx.device.IO_BANK0,
             ctx.device.PADS_BANK0,
@@ -108,6 +119,9 @@ mod app {
             &mut ctx.device.RESETS,
         );
 
+        let pwm_slices = Slices::new(ctx.device.PWM, &mut ctx.device.RESETS);
+
+        // -------------------------- UART TX(gpio0/pin1) RX(gpio1/pin2) --------------------------
         let uart_pins = (
             pins.gpio0.into_function::<gpio::FunctionUart>(),
             pins.gpio1.into_function::<gpio::FunctionUart>(),
@@ -126,19 +140,56 @@ mod app {
         let (uart_rx, uart_tx) = uart.split();
 
         // -------------------------- PWM2 chA gpio4/pin6 --------------------------
-        let pwm_slices = Slices::new(ctx.device.PWM, &mut ctx.device.RESETS);
-
-        let funcPwm = pins.gpio4.into_function::<gpio::FunctionPwm>();
-        let mut pwm = pwm_slices.pwm2;
-        pwm.set_ph_correct();
-        pwm.set_div_int(35);
-        pwm.set_top(PWM_TOP);
-        pwm.enable();
-        let mut channel_a = pwm.channel_a;
-        channel_a.output_to(funcPwm);
+        let pin6 = pins.gpio4.into_function::<gpio::FunctionPwm>();
+        let mut pwm2 = pwm_slices.pwm2;
+        pwm2.set_ph_correct();
+        pwm2.set_div_int(35);
+        pwm2.set_top(PWM_TOP);
+        pwm2.enable();
+        let mut pwm_pin_6 = pwm2.channel_a;
+        pwm_pin_6.output_to(pin6);
         // Set to minimum throttle for Brushless ESC, 30A XT60 Electronic speed regulator,
         // this will make it calibrate to be ready for use.
-        channel_a.set_duty(PWM_TOP / 10);
+        pwm_pin_6.set_duty(PWM_TOP / 10);
+
+        // -------------------------- PWM4 chA gpio8/pin11 --------------------------
+        let pin11 = pins.gpio8.into_function::<gpio::FunctionPwm>();
+        let mut pwm4 = pwm_slices.pwm4;
+        pwm4.set_ph_correct();
+        pwm4.set_div_int(35);
+        pwm4.set_top(PWM_TOP);
+        pwm4.enable();
+        let mut pwm_pin_11 = pwm4.channel_a;
+        pwm_pin_11.output_to(pin11);
+        // Set to minimum throttle for Brushless ESC, 30A XT60 Electronic speed regulator,
+        // this will make it calibrate to be ready for use.
+        pwm_pin_11.set_duty(PWM_TOP / 10);
+
+        // -------------------------- PWM6 chA gpio12/pin16 --------------------------
+        let pin16 = pins.gpio12.into_function::<gpio::FunctionPwm>();
+        let mut pwm6 = pwm_slices.pwm6;
+        pwm6.set_ph_correct();
+        pwm6.set_div_int(35);
+        pwm6.set_top(PWM_TOP);
+        pwm6.enable();
+        let mut pwm_pin_16 = pwm6.channel_a;
+        pwm_pin_16.output_to(pin16);
+        // Set to minimum throttle for Brushless ESC, 30A XT60 Electronic speed regulator,
+        // this will make it calibrate to be ready for use.
+        pwm_pin_16.set_duty(PWM_TOP / 10);
+
+        // -------------------------- PWM0 chA gpio16/pin21 --------------------------
+        let pin21 = pins.gpio16.into_function::<gpio::FunctionPwm>();
+        let mut pwm0 = pwm_slices.pwm0;
+        pwm0.set_ph_correct();
+        pwm0.set_div_int(35);
+        pwm0.set_top(PWM_TOP);
+        pwm0.enable();
+        let mut pwm_pin_21 = pwm0.channel_a;
+        pwm_pin_21.output_to(pin21);
+        // Set to minimum throttle for Brushless ESC, 30A XT60 Electronic speed regulator,
+        // this will make it calibrate to be ready for use.
+        pwm_pin_21.set_duty(PWM_TOP / 10);
 
         // -------------------------- Message Queue --------------------------
         let (s, r) = make_channel!(u8, MSG_Q_CAPACITY);
@@ -146,7 +197,7 @@ mod app {
         let buffer: [u8; UART_READER_CAPACITY] = [0; UART_READER_CAPACITY];
 
         uart_tx.write_full_blocking(
-            b"Write any number 0-5 to adjust pwm. 0 is no throttle and 5 is maximum.\r\n",
+            b"Write any number 0-5 to adjust all 4 pwms. 0 is no throttle and 5 is maximum.\r\n",
         );
 
         esc::spawn().ok();
@@ -154,7 +205,10 @@ mod app {
         (
             Shared {},
             Local {
-                pwm2channel_a: channel_a,
+                pwm_pin_6,
+                pwm_pin_11,
+                pwm_pin_16,
+                pwm_pin_21,
                 buffer,
                 msg_q_sender: s,
                 msg_q_receiver: r,
@@ -184,7 +238,17 @@ mod app {
         }
     }
 
-    #[task(local = [uart_tx, msg_q_receiver, pwm2channel_a], priority = 1)]
+    #[task(
+        local = [
+            uart_tx,
+            msg_q_receiver,
+            pwm_pin_6,
+            pwm_pin_11,
+            pwm_pin_16,
+            pwm_pin_21,
+        ],
+        priority = 1
+    )]
     async fn esc(ctx: esc::Context) {
         let mut buffer: [u8;1] = [0];
         loop {
@@ -197,7 +261,11 @@ mod app {
                         ctx.local
                             .uart_tx
                             .write_full_blocking(b"Updating PWM...\r\n");
-                        ctx.local.pwm2channel_a.set_duty(PWM_TOP / divisor);
+                        ctx.local.pwm_pin_6.set_duty(PWM_TOP / divisor);
+                        ctx.local.pwm_pin_11.set_duty(PWM_TOP / divisor);
+                        ctx.local.pwm_pin_16.set_duty(PWM_TOP / divisor);
+                        ctx.local.pwm_pin_21.set_duty(PWM_TOP / divisor);
+
                     } else {
                         buffer[0] = byte;
                         ctx.local.uart_tx.write_full_blocking(b"Unhandled data: ");
